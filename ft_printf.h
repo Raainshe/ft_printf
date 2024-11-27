c72e73e7b16d80f0ddd7f8f56f45e2416ca311fd
Ryan Makoni <rmakoni@3-F-11.42heilbronn.de>
2024-11-27 15:24:13 +0100

    .h fix

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 12:39:43 by rmakoni           #+#    #+#             */
-/*   Updated: 2024/11/27 15:01:10 by rmakoni          ###   ########.fr       */
+/*   Updated: 2024/11/27 15:19:15 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_printf(const char *format, ...);
-int		ft_putnbr_fdp(int n, int fd);
-int		ft_putchar_fdp(char c, int fd);
-int		ft_putstr_fdp(char *s, int fd);
-int		ft_toupperp(int c, char x);
-int		ft_gethex(int num, char c);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t num, size_t size);
-int		ft_print_unsigned(unsigned int num);
-int		ft_get_address(void *add);
+int	ft_printf(const char *format, ...);
+int	ft_putnbr_fdp(int n, int fd);
+int	ft_putchar_fdp(char c, int fd);
+int	ft_putstr_fdp(char *s, int fd);
+int	ft_toupperp(int c, char x);
+int	ft_gethex(int num, char c);
+int	ft_print_unsigned(unsigned int num);
+int	ft_get_address(void *add);
 #endif
